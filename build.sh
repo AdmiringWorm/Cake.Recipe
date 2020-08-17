4e594383c7c17f2d606b0504fbed64a43010e865
AdmiringWorm <kim.nordmo@gmail.com>
2020-08-18 00:38:20 +0200

    (maint) Update bootstrappers to allow building using Cake.Tool

--- build.sh
+++ build.sh
@@ -7,16 +7,18 @@
 ##########################################################################
 
 # Define directories.
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 TOOLS_DIR=$SCRIPT_DIR/tools
 ADDINS_DIR=$TOOLS_DIR/Addins
 MODULES_DIR=$TOOLS_DIR/Modules
 NUGET_EXE=$TOOLS_DIR/nuget.exe
 CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
+MONO_EXE=mono
 PACKAGES_CONFIG=$TOOLS_DIR/packages.config
 PACKAGES_CONFIG_MD5=$TOOLS_DIR/packages.config.md5sum
 ADDINS_PACKAGES_CONFIG=$ADDINS_DIR/packages.config
 MODULES_PACKAGES_CONFIG=$MODULES_DIR/packages.config
+USE_NETCORE=0
 
 # Define md5sum or md5 depending on Linux/OSX
 MD5_EXE=
@@ -32,92 +34,113 @@ CAKE_ARGUMENTS=()
 
 # Parse arguments.
 for i in "$@"; do
-    case $1 in
-        -s|--script) SCRIPT="$2"; shift ;;
-        --) shift; CAKE_ARGUMENTS+=("$@"); break ;;
-        *) CAKE_ARGUMENTS+=("$1") ;;
+    case $(echo "$1" | tr '[:upper:]' '[:lower:]') in
+    -s | --script)
+        SCRIPT="$2"
+        shift
+        ;;
+    --usenetcore)
+        echo "Enabling .NET Core build"
+        USE_NETCORE=1
+        shift
+        ;;
+    --)
+        shift
+        CAKE_ARGUMENTS+=("$@")
+        break
+        ;;
+    *)
+        CAKE_ARGUMENTS+=("$1")
+        echo "Using argument $1"
+        ;;
     esac
     shift
 done
 
-# Make sure the tools folder exist.
-if [ ! -d "$TOOLS_DIR" ]; then
-  mkdir "$TOOLS_DIR"
-fi
+if [ "$USE_NETCORE" -eq "1" ]; then
+    dotnet tool restore
+    MONO_EXE=dotnet
+    CAKE_EXE=cake
+else
+    # Make sure the tools folder exist.
+    if [ ! -d "$TOOLS_DIR" ]; then
+        mkdir "$TOOLS_DIR"
+    fi
 
-# Make sure that packages.config exist.
-if [ ! -f "$TOOLS_DIR/packages.config" ]; then
-    echo "Downloading packages.config..."
-    curl -Lsfo "$TOOLS_DIR/packages.config" https://cakebuild.net/download/bootstrapper/packages
+    # Make sure that packages.config exist.
+    if [ ! -f "$TOOLS_DIR/packages.config" ]; then
+        echo "Downloading packages.config..."
+        curl -Lsfo "$TOOLS_DIR/packages.config" https://cakebuild.net/download/bootstrapper/packages
+        if [ $? -ne 0 ]; then
+            echo "An error occurred while downloading packages.config."
+            exit 1
+        fi
+    fi
+
+    # Download NuGet if it does not exist.
+    if [ ! -f "$NUGET_EXE" ]; then
+        echo "Downloading NuGet..."
+        curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+        if [ $? -ne 0 ]; then
+            echo "An error occurred while downloading nuget.exe."
+            exit 1
+        fi
+    fi
+
+    # Restore tools from NuGet.
+    pushd "$TOOLS_DIR" >/dev/null
+    if [ ! -f "$PACKAGES_CONFIG_MD5" ] || [ "$(cat "$PACKAGES_CONFIG_MD5" | sed 's/\r$//')" != "$($MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }')" ]; then
+        find . -type d ! -name . ! -name 'Cake.Bakery' | xargs rm -rf
+    fi
+
+    . $MONO_EXE "$NUGET_EXE" install -ExcludeVersion
     if [ $? -ne 0 ]; then
-        echo "An error occurred while downloading packages.config."
+        echo "Could not restore NuGet tools."
         exit 1
     fi
-fi
 
-# Download NuGet if it does not exist.
-if [ ! -f "$NUGET_EXE" ]; then
-    echo "Downloading NuGet..."
-    curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-    if [ $? -ne 0 ]; then
-        echo "An error occurred while downloading nuget.exe."
-        exit 1
-    fi
-fi
-
-# Restore tools from NuGet.
-pushd "$TOOLS_DIR" >/dev/null
-if [ ! -f "$PACKAGES_CONFIG_MD5" ] || [ "$( cat "$PACKAGES_CONFIG_MD5" | sed 's/\r$//' )" != "$( $MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' )" ]; then
-    find . -type d ! -name . ! -name 'Cake.Bakery' | xargs rm -rf
-fi
-
-mono "$NUGET_EXE" install -ExcludeVersion
-if [ $? -ne 0 ]; then
-    echo "Could not restore NuGet tools."
-    exit 1
-fi
-
-$MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' >| "$PACKAGES_CONFIG_MD5"
-
-popd >/dev/null
-
-# Restore addins from NuGet.
-if [ -f "$ADDINS_PACKAGES_CONFIG" ]; then
-    pushd "$ADDINS_DIR" >/dev/null
-
-    mono "$NUGET_EXE" install -ExcludeVersion
-    if [ $? -ne 0 ]; then
-        echo "Could not restore NuGet addins."
-        exit 1
-    fi
+    $MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' >|"$PACKAGES_CONFIG_MD5"
 
     popd >/dev/null
-fi
 
-# Restore modules from NuGet.
-if [ -f "$MODULES_PACKAGES_CONFIG" ]; then
-    pushd "$MODULES_DIR" >/dev/null
+    # Restore addins from NuGet.
+    if [ -f "$ADDINS_PACKAGES_CONFIG" ]; then
+        pushd "$ADDINS_DIR" >/dev/null
 
-    mono "$NUGET_EXE" install -ExcludeVersion
-    if [ $? -ne 0 ]; then
-        echo "Could not restore NuGet modules."
-        exit 1
+        . $MONO_EXE "$NUGET_EXE" install -ExcludeVersion
+        if [ $? -ne 0 ]; then
+            echo "Could not restore NuGet addins."
+            exit 1
+        fi
+
+        popd >/dev/null
     fi
 
-    popd >/dev/null
-fi
+    # Restore modules from NuGet.
+    if [ -f "$MODULES_PACKAGES_CONFIG" ]; then
+        pushd "$MODULES_DIR" >/dev/null
 
-# Make sure that Cake has been installed.
-if [ ! -f "$CAKE_EXE" ]; then
-    echo "Could not find Cake.exe at '$CAKE_EXE'."
-    exit 1
+        . $MONO_EXE "$NUGET_EXE" install -ExcludeVersion
+        if [ $? -ne 0 ]; then
+            echo "Could not restore NuGet modules."
+            exit 1
+        fi
+
+        popd >/dev/null
+    fi
+
+    # Make sure that Cake has been installed.
+    if [ ! -f "$CAKE_EXE" ]; then
+        echo "Could not find Cake.exe at '$CAKE_EXE'."
+        exit 1
+    fi
 fi
 
 if [ -f "./includes.cake" ]; then
     rm -f "./includes.cake"
 fi
 
-find ./Cake.Recipe/Content -type f \( -iname "*.cake" -and -not -iname "version.cake" \) -exec echo "#load \"local:?path={}\"" >> includes.cake \;
+find ./Cake.Recipe/Content -type f \( -iname "*.cake" -and -not -iname "version.cake" \) -exec echo "#load \"local:?path={}\"" \; >>includes.cake
 
 # Start Cake
-exec mono "$CAKE_EXE" $SCRIPT "${CAKE_ARGUMENTS[@]}"
+exec $MONO_EXE "$CAKE_EXE" $SCRIPT "${CAKE_ARGUMENTS[@]}"
